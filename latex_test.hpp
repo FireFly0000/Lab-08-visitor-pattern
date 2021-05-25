500e97a94b67a4f103e767628c4113d62297e649
joeypercia <jperc004@ucr.edu>
2021-05-25 00:26:52 -0700

    Fix tests

--- latex_test.hpp
+++ latex_test.hpp
@@ -12,49 +12,51 @@
 #include "visitLaTex.hpp"
 #include "iterator.hpp"
 #include <sstream>
+#include "rand.hpp"
+#include "iterator.cpp"
 
 TEST(LatexTest, AddingValues) {
     std::stringstream os;
-    VisitLatex* v;
+    VisitLatex v;
     Base* two = new Op(2);
     Base* five = new Op(5);
     Base* add = new Add(two, five);
 
-    os << v->PrintLaTeX(add);
+    os << v.PrintLaTeX(add);
 
     EXPECT_EQ(os.str(), "${({2}+{5})}$");
 }
 
 TEST(LatexTest, AddingandSubtracting) {
     std::stringstream os;
-    VisitLatex* v;
+    VisitLatex v;
     Base* two = new Op(2);
     Base* five = new Op(5);
     Base* seven = new Op(7);
     Base* add = new Add(two, five);
     Base* min = new Sub(add, seven);
 
-    os << v->PrintLaTeX(min);
+    os << v.PrintLaTeX(min);
 
     EXPECT_EQ(os.str(), "${({({2}+{5})}-{7})}$");
 }
 
 TEST(LatexTest, MultandDiv) {
     std::stringstream os;
-    VisitLatex* v;
+    VisitLatex v;
     Base* two = new Op(2);
     Base* five = new Op(5);
     Base* mult = new Mult(two, five);
     Base* div = new Div(mult, two);
 
-    os << v->PrintLaTeX(div);
+    os << v.PrintLaTeX(div);
 
     EXPECT_EQ(os.str(), "${\\frac{({2}\\cdot{5})}{2}}$");
 }
 
 TEST(LatexTest, AddMultPow) {
     std::stringstream os;
-    VisitLatex* v;
+    VisitLatex v;
     Base* two = new Op(2);
     Base* five = new Op(5);
     Base* three = new Op(3);
@@ -62,20 +64,20 @@ TEST(LatexTest, AddMultPow) {
     Base* mult = new Mult(add, two);
     Base* pow = new Pow(mult, three);
 
-    os << v->PrintLaTeX(pow);
+    os << v.PrintLaTeX(pow);
 
     EXPECT_EQ(os.str(), "$({{({({5}+{3})}\\cdot{2})}^{3})}$");
 }
 TEST(LatexTest, NegativeMultDiv) {
     std::stringstream os;
-    VisitLatex* v;
+    VisitLatex v;
     Base* two = new Op(-2);
     Base* five = new Op(5);
     Base* three = new Op(-3);
     Base* mult = new Mult(five, two);
     Base* div = new Div(mult, three);
 
-    os << v->PrintLaTeX(div);
+    os << v.PrintLaTeX(div);
 
     EXPECT_EQ(os.str(), "${\\frac{({5}\\cdot{-2})}{-3}}$");
 }
